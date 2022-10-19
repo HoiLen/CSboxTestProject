01094adfe5fb60e03483050feda2cc044c8f63a5
HoiLen <80934491+HoiLen@users.noreply.github.com>
2022-10-19 14:13:39 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//<App />の中身
+const App = () => {
+  return <h1>こんちは～</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
